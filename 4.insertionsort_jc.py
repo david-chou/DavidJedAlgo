aa0a46e1621afd6fd6952b94b9b7265a529fa186
Jed Chou <jedchou1@illinois.edu>
2015-08-20 23:16:01 -0500

    fixed bugs in insertion sort

--- 4.insertionsort_jc.py
+++ 4.insertionsort_jc.py
@@ -3,11 +3,11 @@
 def insertionsort(input_array):
     n = len(input_array)
     #sorts the array in place
-    for i in range(0,n):
+    for i in range(1,n):
         key = input_array[i]
         j = i -1
-        #compare current key to the sorted array from index 0 to index i-1
-        while (j > 0 and input_array[j] > key):
+        #compare current key to the sorted subarray to the left of key
+        while (j >= 0 and input_array[j] > key):
             input_array[j+1] = input_array[j]
             j -= 1
         input_array[j+1] = key
@@ -15,3 +15,4 @@ def insertionsort(input_array):
 
 print insertionsort([1,3,4,5,2,6,7,8,1,2,13])
 print insertionsort([1,2,3,4,5])
+print insertionsort([5,4,3,2,1])
